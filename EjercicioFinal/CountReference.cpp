cd49b4eafb40061b7621eeaacb6942cd143144c3
kal3bo <a01018992@itesm.mx>
2017-05-03 09:50:31 -0500

    Update CountReference.cpp

--- EjercicioFinal/CountReference.cpp
+++ EjercicioFinal/CountReference.cpp
@@ -1,46 +1,155 @@
+//Diego Kaleb Valenzuela Carrillo
+//A01018992
 #include <iostream>
+#include <string>
+
 using namespace std;
 
-class Counter{
-    public:
-    int count;
-    Counter(){count = 1;}
-    void increaseCounter(){count++;}
-    void decreaseCounter(){count--;}
-   
-    int getCountInCount(){return count;}                        //Test purposes
-};
+class Visitor;
+class Asteroid;
+class Spacecrafts;
+class Planet;
 
-class Child : public Counter{
-   private:
-   int *p;
-   
-   public:
-   Child(int x){              //Normal constructor
-       p = new int;                                             //When we create a new obj of type child 
-       *p = x;
-   }
-   Child(const Child &obj){  //Copy Constructor
-     p = new int;
-     *p = *obj.p;
-     increaseCounter();                                         //When we make a copy of the last child, there are two childs having the same pointer, so we increase the counter by 1
-   }
-   ~Child(){                  //Destructor
-       decreaseCounter();
-       if (count == 0){                                         //This ensures that the pointer is deleted only when no one else is using it
-         cout << "Freeing memory..." << endl;
-         delete p;
-       }
-   }
-   int getCountInChild(){return getCountInCount();}
-   int getP(){return *p;}
+class Spacecrafts {
+protected:
+    string queSoy;
+public:
+    string getSpace() { return queSoy; }
+    virtual void accept(Visitor*) = 0;
+    void getAsteroid(Asteroid *a);
+    void getPlanet(Planet *p);
 };
-
-int main(){
-    Child a(5);
-    cout << "Number of Counts: " << a.getCountInChild() << endl;
-    Child b = a;
-    cout << "Number of Counts: " << b.getCountInChild() << endl;
-    cout << "The value in Child B: " << b.getP() << endl;
+class PlanetExplorationShips : public Spacecrafts {
+public:
+    PlanetExplorationShips() { queSoy = "Planet Exploration Ship"; }
+    void accept(Visitor*);
+};
+class ColonizationShips : public Spacecrafts {
+public:
+    ColonizationShips() { queSoy = "Colonization Ship"; }
+    void accept(Visitor*);
+};
+class PlanetObservationShips : public Spacecrafts {
+public:
+    PlanetObservationShips() { queSoy = "Observation Ship"; }
+    void accept(Visitor*);
+};
+////////////////////////////////////////////////////////////////////////////////////////////
+class Asteroid {
+protected:
+    string queAsteroidSoy;
+public:
+    string getAsteroid() { return queAsteroidSoy; }
+    virtual void accept(Visitor*) = 0;
+    void getSpace(Spacecrafts *s);
+    void getPlanet(Planet *p);
+};
+class StonyMeteorites : public Asteroid {
+public:
+    StonyMeteorites() { queAsteroidSoy = "Stony Meteorite"; }
+    void accept(Visitor*);
+};
+class IonMeteorites : public Asteroid {
+public:
+    IonMeteorites() { queAsteroidSoy = "Ion Meteorite"; }
+    void accept(Visitor*);
+};
+////////////////////////////////////////////////////////////////////////////////////////////
+class Planet{
+protected:
+    string quePlanetaSoy;
+public:
+    string getPlanet() { return quePlanetaSoy; }
+    virtual void accept(Visitor*) = 0;
+    void getSpace(Spacecrafts *s);
+    void getAsteroid(Asteroid *a);
+};
+class DesertPlanet : public Planet{
+public:
+    DesertPlanet() { quePlanetaSoy = "Desert Planet"; }
+    void accept(Visitor*);
+};
+class EarthAnalog : public Planet{
+public:
+    EarthAnalog() { quePlanetaSoy = "Earth Analog"; }
+    void accept(Visitor*);
+};
+////////////////////////////////////////////////////////////////////////////////////////////
+class Visitor {
+public:
+    virtual void visit(PlanetExplorationShips*) = 0;
+    virtual void visit(ColonizationShips*) = 0;
+    virtual void visit(PlanetObservationShips*) = 0;
     
+    virtual void visit(StonyMeteorites*) = 0;
+    virtual void visit(IonMeteorites*) = 0;
+    
+    virtual void visit(DesertPlanet*) = 0;
+    virtual void visit(EarthAnalog*) = 0;
+    //Singleton
+    template <class T>
+    static Visitor* getInstance() {
+        static T instance;
+        return &instance;
+    }
+};
+class PresentVisitor : public Visitor {
+    void visit(PlanetExplorationShips* PES) { cout << PES->getSpace(); }
+    void visit(ColonizationShips* CS) { cout << CS->getSpace(); }
+    void visit(PlanetObservationShips* POS) { cout << POS->getSpace(); }
+    
+    void visit(StonyMeteorites* SM) { cout << SM->getAsteroid(); }
+    void visit(IonMeteorites* IM) { cout << IM->getAsteroid(); }
+    
+    void visit(DesertPlanet* DP) { cout << DP->getPlanet(); }
+    void visit(EarthAnalog* EA) { cout << EA->getPlanet(); }
+};
+void PlanetExplorationShips::accept(Visitor* v) { v->visit(this); }
+void ColonizationShips::accept(Visitor* v) { v->visit(this); }
+void PlanetObservationShips::accept(Visitor* v) { v->visit(this); }
+
+void StonyMeteorites::accept(Visitor* v) { v->visit(this); }
+void IonMeteorites::accept(Visitor* v) { v->visit(this); }
+
+void DesertPlanet::accept(Visitor* v) { v->visit(this); }
+void EarthAnalog::accept(Visitor* v) { v->visit(this); }
+
+//
+void Spacecrafts::getAsteroid(Asteroid *a) { cout << " collisions with a " << a->getAsteroid(); }
+void Spacecrafts::getPlanet(Planet *p) { cout << " collisions with a " << p->getPlanet(); }
+
+void Asteroid::getSpace(Spacecrafts *s) { cout << " collisions with a " << s->getSpace(); }
+void Asteroid::getPlanet(Planet *p) { cout << " collisions with a " << p->getPlanet(); }
+
+void Planet::getSpace(Spacecrafts *s) { cout << " collisions with a " << s->getSpace(); }
+void Planet::getAsteroid(Asteroid *a) { cout << " collisions with a " << a->getAsteroid(); }
+////////////////////////////////////////////////////////////////////////////////////////////
+int main() {
+    
+    Visitor* V = Visitor::getInstance<PresentVisitor>();
+    Spacecrafts* S[] = { new PlanetExplorationShips, new ColonizationShips, new PlanetObservationShips };
+    Asteroid* A[] = { new StonyMeteorites, new IonMeteorites };
+    Planet* P[] = {new DesertPlanet, new EarthAnalog };
+    
+    S[0]->accept(V);
+    S[0]->getAsteroid(A[0]);
+    cout << endl;
+    S[1]->accept(V);
+    S[1]->getAsteroid(A[1]);
+    cout << endl;
+    S[2]->accept(V);
+    S[2]->getPlanet(P[0]);
+    cout << endl;
+    S[0]->accept(V);
+    S[0]->getPlanet(P[1]);
+    cout << endl;
+    A[0]->accept(V);
+    A[0]->getSpace(S[0]);
+    cout << endl;
+    P[0]->accept(V);
+    P[0]->getAsteroid(A[0]);
+    cout << endl;
+    
+    return 0;
 }
+
